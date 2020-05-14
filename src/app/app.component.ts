0cf25d08260167408e03693a37480304c5165621
adzamkomladev <adzamkomla.dev@gmail.com>
2020-05-14 20:58:35 +0000

    add validation to forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-student-verification-system';
+  image: File;
+  previewImage: string;
+
+  constructor() {
+    this.previewImage = '/assets/img/nsvs1.jpg';
+  }
+
+  onFileUpload(event): void {
+    const [file] = event.target.files;
+
+    const reader = new FileReader();
+    reader.onload = () => (this.previewImage = reader.result as string);
+    reader.readAsDataURL(file);
+
+    this.image = file;
+  }
+
+  onSubmit(form: NgForm): void {}
 }
